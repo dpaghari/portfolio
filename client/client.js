3b2f7534ba054863e29d38de611528ec8d51b956
Daniel Pagharion <daniel.pagharion@tightropeinteractive.com>
2017-04-18 16:58:14 -0700

    Push to github

--- client/client.js
+++ client/client.js
@@ -1,12 +1,19 @@
-// const Waypoint = require('waypoints');
-
-
 $(document).ready(function (){
   let $navIcon = $('.nav-icon');
   $navIcon.on('click', function () {
     $(this).toggleClass('open');
     $('.menu-container').toggleClass('active');
   });
+
+  let $splash = document.querySelector('section.hero');
+  let heroWaypoint = new Waypoint({
+    element: $splash,
+    handler: function(direction) {
+      if(direction === 'down')
+      $splash.classList.add('wp-fadeup');
+    }
+  });
+
 
   let $project = document.querySelector('section.projects');
   let projectWaypoint = new Waypoint({
@@ -18,68 +25,20 @@ $(document).ready(function (){
     offset: '35%'
   });
   $('a[href^="#"]').on('click',function (e) {
-  	    e.preventDefault();
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
+  });
 
-  	    var target = this.hash;
-  	    var $target = $(target);
-        console.log($target.offset().top);
-  	    $('html, body').stop().animate({
-  	        'scrollTop': $target.offset().top - 85
-  	    }, 900, 'swing');
-  	});
-    // function triggered on window scroll
-  // $(window).on('scroll', function() {
-  //
-  //     // store the current vertical position of the scroll bar
-  //     var scroll_top = $(window).scrollTop();
-  //
-  //     // change inline css rule, based on factor of scroll_top
-  //     $('h1.headline').css('background-position-y', scroll_top * -1.0);
-  // });
+  $(document).on('click', '.nerd-note-btn', function(e) {
+    e.preventDefault();
+    let $nerdNote = $(this).siblings('.nerd-notes')[0];
+    console.log($nerdNote);
+    // $($nerdNote).toggleClass('active');
+    $($nerdNote).slideToggle('fast');
+
+  });
 });
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//
-//   let navIcon = document.querySelector('.nav-icon');
-//
-//   navIcon.addEventListener('click', function (e) {
-//     this.classList.toggle('open');
-//     document.querySelector('.menu-container').classList.toggle('active');
-//   });
-//
-//   let $project = document.querySelector('section.projects');
-//   let projectWaypoint = new Waypoint({
-//     element: $project,
-//     handler: function(direction) {
-//       if(direction === 'down')
-//       $project.classList.add('wp-fadein');
-//     },
-//     offset: '35%'
-//   });
-//
-//   // document.addEventListener('click', function (e) {
-//   //   console.log(e.target);
-//   //   if(e.target.classList.contains('arrow-down')) {
-//   //     window.location = '#about';
-//   //   }
-//   // });
-//
-//   $('a[href^="#"]').on('click',function (e) {
-//   	    e.preventDefault();
-//
-//   	    var target = this.hash;
-//   	    var $target = $(target);
-//
-//   	    $('html, body').stop().animate({
-//   	        'scrollTop': $target.offset().top
-//   	    }, 900, 'swing');
-//   	});
-//
-//
-// });
